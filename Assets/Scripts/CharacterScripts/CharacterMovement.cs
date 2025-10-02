969e5dd5a045543f71d9726985d9c55d7a3f7279
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-10-02 16:16:16 -0300

    add NETWORKED player movement

--- Assets/Scripts/CharacterScripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterScripts/CharacterMovement.cs
@@ -1,7 +1,34 @@
 using Fusion;
+using Fusion.Addons.SimpleKCC;
 using UnityEngine;
 
 public class CharacterMovement : NetworkBehaviour
 {
-    
+    [SerializeField] private SimpleKCC kcc; //kcc: kinematic character controller
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpPower = 10f;
+
+    [Networked] private NetworkButtons PreviousButtons { get; set; }
+
+    public override void Spawned()
+    {
+        kcc.SetGravity(Physics.gravity.y * 2f);
+    }
+
+    public override void FixedUpdateNetwork() //executing logic that affects gameplay
+    {
+        if (GetInput(out NetInputPlayer input)) //gets the input of each client
+        {
+            Vector3 worldDirection = kcc.TransformRotation * new Vector3(input.Direction.x, 0f, input.Direction.y); //take the kcc transform rotation and we multiply it by the direction of the input
+            float jump = 0f;
+
+            if (input.Buttons.WasPressed(PreviousButtons, InputButton.Jump) && kcc.IsGrounded) //check if player pressed jump button and is grounded
+            {
+                jump = jumpPower;
+            }
+
+            kcc.Move(worldDirection.normalized * speed, jump); //normalizing the wD vector to prevent cheating
+            PreviousButtons = input.Buttons;
+        }  
+    }
 }
