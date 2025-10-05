0917bf6ea1169e6b263bd4e6cc785b36b8ea4fab
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-10-05 11:47:28 -0300

    testing scene and prefab

--- Assets/Scripts/CharacterScripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterScripts/CharacterMovement.cs
@@ -8,17 +8,27 @@ public class CharacterMovement : NetworkBehaviour
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpPower = 10f;
 
+    [SerializeField] private Transform camTarget; //Cam Holder 
+
     [Networked] private NetworkButtons PreviousButtons { get; set; }
 
     public override void Spawned()
     {
         kcc.SetGravity(Physics.gravity.y * 2f);
+
+        if (HasInputAuthority)
+        {
+            CameraFollow.Instance.SetTarget(camTarget);
+        }
     }
 
     public override void FixedUpdateNetwork() //executing logic that affects gameplay
     {
         if (GetInput(out NetInputPlayer input)) //gets the input of each client
         {
+
+
+
             Vector3 worldDirection = kcc.TransformRotation * new Vector3(input.Direction.x, 0f, input.Direction.y); //take the kcc transform rotation and we multiply it by the direction of the input
             float jump = 0f;
 
@@ -29,6 +39,18 @@ public class CharacterMovement : NetworkBehaviour
 
             kcc.Move(worldDirection.normalized * speed, jump); //normalizing the wD vector to prevent cheating
             PreviousButtons = input.Buttons;
-        }  
+        }
     }
+
+
+    public override void Render()
+    {
+        UpdateCamTarget();
+    }
+
+    private void UpdateCamTarget()
+    {
+        camTarget.localPosition = transform.position;
+    }
+
 }
