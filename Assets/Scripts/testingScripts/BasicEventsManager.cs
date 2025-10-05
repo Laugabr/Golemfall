56f96b841be2b7c0bd587f88659c64222176f5dc
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-10-05 12:49:46 -0300

    add basic events script and coin scripts

---
+++ Assets/Scripts/testingScripts/BasicEventsManager.cs
@@ -0,0 +1,7 @@
+using System;
+using UnityEngine;
+
+public static class BasicEventsManager
+{
+        public static Action<int> OnExperienceGain;
+}
--- Assets/Scripts/testingScripts/CameraFollow.cs
+++ Assets/Scripts/testingScripts/CameraFollow.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class Camera1Follow : MonoBehaviour
 {
     [SerializeField] private Transform target;  // El Player
     [SerializeField] private Vector3 offset = new Vector3(0f, 5f, -7f);
