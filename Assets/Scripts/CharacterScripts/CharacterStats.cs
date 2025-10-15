c9b060b4b17150172f45fc7cb87743acb33284c7
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-10-14 23:34:23 -0300

    Add stats system [WIP]

---
+++ Assets/Scripts/CharacterScripts/CharacterStats.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterStats : MonoBehaviour
+{
+    [Header("Base Stats")]
+    public Stats baseStats;
+
+    [Header("Runtime Stats")]
+    public List<StatInfo> localStats = new List<StatInfo>();
+    public virtual void Initialize()
+    {
+        // Copiamos los valores base
+        localStats = new List<StatInfo>(
+            baseStats.statInfo.Select(s => new StatInfo(s.statType, s.statValue))
+        );
+    }
+
+    public int GetStat(Stat stat)
+    {
+        return localStats.FirstOrDefault(s => s.statType == stat)?.statValue ?? 0;
+    }
+
+}
