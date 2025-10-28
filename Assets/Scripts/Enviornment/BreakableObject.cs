35be6443d9dc65a21054bc6d658f9d18635e44de
Lucianatux <lucianatux@yahoo.com.ar>
2025-10-28 17:20:43 -0300

    add max level to experience
    integrate experience with breakable objects , add UI

--- Assets/Scripts/Enviornment/BreakableObject.cs
+++ Assets/Scripts/Enviornment/BreakableObject.cs
@@ -6,6 +6,8 @@ public class BreakableObject : MonoBehaviour
     [Header("Configuración")]
     [SerializeField] private int hitsToBreak = 3;
     [SerializeField] private GameObject breakEffectPrefab;
+    [SerializeField] private int experienceReward = 1;
+    [SerializeField] private HitFeedback hitFeedback;
 
     private int currentHits = 0;
     private bool isBroken = false;
@@ -32,14 +34,15 @@ public class BreakableObject : MonoBehaviour
     {
         isBroken = true;
 
-        if (breakEffectPrefab)
-            Instantiate(breakEffectPrefab, transform.position, Quaternion.identity);
+        if (breakEffectPrefab) Instantiate(breakEffectPrefab, transform.position, Quaternion.identity);
 
         // Buscar automáticamente BreakableDrop en el mismo objeto
         BreakableDrop drop = GetComponent<BreakableDrop>();
-        if (drop != null)
-            drop.SpawnDrop(transform.position);
+        if (drop != null) drop.SpawnDrop(transform.position);
 
+        BasicEventsManager.OnExperienceGain?.Invoke(experienceReward);
+        Debug.Log($"{gameObject.name} se rompió. +{experienceReward} EXP");
         Destroy(gameObject);
+
     }
 }
