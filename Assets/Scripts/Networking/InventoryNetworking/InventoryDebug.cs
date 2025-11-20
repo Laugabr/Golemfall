51e173aafa5e057d1215cb87ef3c877f2e697a35
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-11-20 01:19:31 -0300

    WIP player prefab add scripts Inv. Debugger

--- Assets/Scripts/Networking/InventoryNetworking/InventoryDebug.cs
+++ Assets/Scripts/Networking/InventoryNetworking/InventoryDebug.cs
@@ -1,8 +1,11 @@
+using System.Linq;
+using Fusion;
 using UnityEngine;
 
-public class InventoryDebug : MonoBehaviour
+public class InventoryDebug : NetworkBehaviour
 {
     [SerializeField] NetworkInventory inv;
+    [SerializeField] PlayerStats stats;
 
     private void Update()
     {
@@ -13,6 +16,79 @@ public class InventoryDebug : MonoBehaviour
             foreach (var it in inv.Items)
                 Debug.Log(" - " + it);
         }
+
+    }
+}public class InventoryDebugTester : NetworkBehaviour
+{
+    private NetworkInventory inv;
+    private PlayerStats stats;
+
+    private void Awake()
+    {
+        inv = GetComponent<NetworkInventory>();
+        stats = GetComponent<PlayerStats>();
+    }
+
+    void Update()
+    {
+        // Solo el dueño puede debuggear su propio personaje
+        if (!Object.HasInputAuthority) return;
+
+        // Mostrar items
+        if (Input.GetKeyDown(KeyCode.I))
+            Debug_ShowInventory();
+
+        // Equipar todo
+        //if (Input.GetKeyDown(KeyCode.G))
+
+        // Desequipar todo
+        if (Input.GetKeyDown(KeyCode.U))
+            Debug_UnequipAll();
+
+        // Debug de stats
+        if (Input.GetKeyDown(KeyCode.P))
+        {           
+            
+            Debug.LogError("equipment requested ");
+
+            Debug_ShowStats();            
+
+            Debug_EquipAll();
+            }
+    }
+
+    private void Debug_ShowInventory()
+    {
+        Debug.Log($"[{Object.InputAuthority}] INVENTORY → {string.Join(", ", inv.Items)}");
+        Debug.Log($"[{Object.InputAuthority}] EQUIPPED → {string.Join(", ", inv.EquipedItems)}");
+    }
+
+    private void Debug_ShowStats()
+    {
+        string s = $"[{Object.InputAuthority}] STATS: ";
+        foreach (var st in stats.localStats)
+            s += $"{st.statType}={st.statValue} ";
+
+        Debug.Log(s);
+    }
+
+    private void Debug_EquipAll()
+    {
+        foreach (var id in inv.Items)
+        {        
+            Debug.Log(id);
+
+            inv.RPC_ServerEquipmentRequest(id);}
+
+        Debug.Log($"[{Object.InputAuthority}] Equipado TODO");
+    }
+
+    private void Debug_UnequipAll()
+    {
+        foreach (var id in inv.EquipedItems.ToList())
+            inv.RPC_ServerUnequipmentRequest(id);
+
+        Debug.Log($"[{Object.InputAuthority}] Desequipado TODO");
     }
 }
 
