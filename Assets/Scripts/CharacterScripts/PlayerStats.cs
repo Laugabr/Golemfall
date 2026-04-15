0428a34ec85a854583156e9c0f4ff7c60926a9d7
Laugabr <125941878+Laugabr@users.noreply.github.com>
2026-04-14 21:22:06 -0300

    fix item pick up

--- Assets/Scripts/CharacterScripts/PlayerStats.cs
+++ Assets/Scripts/CharacterScripts/PlayerStats.cs
@@ -44,7 +44,7 @@ public class PlayerStats : CharacterStats
         Debug.Log(localStats.FirstOrDefault(s => s.statType == Stat.speed));
 
         localStats.Clear();
-        foreach (var bs in baseLevelStats)
+        foreach (var bs in baseStats.statInfo)
         {
             var existing = localStats.FirstOrDefault(s => s.statType == bs.statType);
             if (existing != null) existing.statValue += bs.statValue;
