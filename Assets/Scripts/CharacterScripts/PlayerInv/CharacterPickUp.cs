6b37c16d390a64bc8fb85951a08b83e94e549728
Lucianatux <lucianatux@yahoo.com.ar>
2026-04-21 22:03:32 -0300

    invoke collectItem event

--- Assets/Scripts/CharacterScripts/PlayerInv/CharacterPickUp.cs
+++ Assets/Scripts/CharacterScripts/PlayerInv/CharacterPickUp.cs
@@ -37,6 +37,7 @@ public class CharacterPickUp : NetworkBehaviour
 
         Debug.Log($"{Object} tries to collect {item.Item.displayName}");
         item.Rpc_Collect(Object);
+        TrackEvents.OnTrackEvent?.Invoke(GameEventType.CollectItem, 1);
 
         InteractPrompt.Instance?.Hide();
         MessageManager.Instance?.Show("Item recolectado");
