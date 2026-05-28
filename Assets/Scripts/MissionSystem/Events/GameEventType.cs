1e4787e1d944878178adfba99db74ea3c1d54cd8
Lucianatux <lucianatux@yahoo.com.ar>
2026-05-28 20:27:23 -0300

    setting tex on dialogues

--- Assets/Scripts/MissionSystem/Events/GameEventType.cs
+++ Assets/Scripts/MissionSystem/Events/GameEventType.cs
@@ -5,10 +5,19 @@ public enum GameEventType
     KillEnemy,
     CollectItem,
     CollectSpecialItem,
+    CollectSpecialItem1,
     TalkNPC,
+    TalkNPC1,
+    TalkNPC2,
+    TalkNPC3,
+    TalkNPC4,
+    TalkNPC5,
     KillNPC,
     EnterCave,
     PlayerDied,
     BreakBreakable,
-    MisionGoldenEgg
+    BreakSpecialBreakable1,
+    StartMisionGoldenEgg,
+    StartMisionRompibles1,
+    StartMisionRompibles2
 }
