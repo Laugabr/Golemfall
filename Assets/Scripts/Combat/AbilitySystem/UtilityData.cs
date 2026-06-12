f7f1cbb9a9a2c24158d7d3d1842f10271248ec0c
Laugabr <125941878+Laugabr@users.noreply.github.com>
2026-06-12 19:14:52 -0300

    fix uData

--- Assets/Scripts/Combat/AbilitySystem/UtilityData.cs
+++ Assets/Scripts/Combat/AbilitySystem/UtilityData.cs
@@ -6,5 +6,4 @@ public class UtilityAbilityData : Ability
     public GameObject aoePrefab;
     public float aoeRadius = 5f;
     public float healMultiplier = 1f; // heal = damage * healMultiplier
-    public float activeTime = 2f;     // cuánto dura el AOE visible
 }
