08e6f1e7d5144ee817d48207aab9a8682c2f82a4
Laugabr <125941878+Laugabr@users.noreply.github.com>
2026-07-02 23:55:03 -0300

    chore: Optimize enemy health bar by culling it by distance

--- Assets/Scripts/UI/EnemyHealthBar.cs
+++ Assets/Scripts/UI/EnemyHealthBar.cs
@@ -5,19 +5,34 @@ public class EnemyHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private Transform billboard; // el canvas root
-    
+    private Renderer enemyRenderer; 
+    private float maxBillboardDistance = 30f;
     private Camera mainCam;
 
     private void Awake()
     {
         mainCam = Camera.main;
+        Renderer rend = GetComponentInParent<Renderer>();
+
+        if (rend != null)
+            enemyRenderer = rend;
+
+
+        maxBillboardDistance = GetComponentInParent<EnemyAI>().VisionRange;
     }
+
+
 
     private void LateUpdate()
     {
-        // Siempre mira a la cámara
-        if (mainCam != null && billboard != null)
-            billboard.rotation = mainCam.transform.rotation;
+        if (mainCam == null || billboard == null) return;
+
+        // Salta el trabajo si está lejos o fuera de cámara
+        float sqrDist = (transform.position - mainCam.transform.position).sqrMagnitude;
+        if (sqrDist > maxBillboardDistance * maxBillboardDistance) return;
+        if (enemyRenderer != null && !enemyRenderer.isVisible) return;
+
+        billboard.rotation = mainCam.transform.rotation;
     }
 
     public void SetMaxHealth(int max)
